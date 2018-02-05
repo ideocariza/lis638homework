198cb2a915f2f3b08bfdb8d120ff3dc7d80c07d7
Izzy <ideocariza@gmail.com>
2018-02-04 23:48:43 -0500

    HW1 - 1.1. Challenge 1 - Pseudocode
    HW1 - Challenge 1
    Pseudocode laid down

---
+++ codehw1.php
@@ -0,0 +1,58 @@
+<html>
+<body>
+
+    
+<!-- CHALLENGE 1 ----------------------------------------------------------------- -->
+    
+    
+<?php
+    
+        /* Step 1. Define the change due in cents, and create a variable for each denomination of change
+        */
+  
+    
+        /* Step 2. Subtract 100 from the change due as many times as that can be done, and count how many times that is as the number dollars due. Once denominations of 100 can no longer be subtracted from the change due, the remaining value of the change due is pushed to step 3.
+        */ 
+        
+        /* Step 3. Subtract 25 from the remaining change due as many times as that can be done, and count how many times that is as the number of  quarters due. Once denominations of 25 can no longer be subtracted from the change due, the remaining value of the change due is pushed to step 4.
+        
+        /* Step 4. Subtract 10 from the remaining change due as many times as that can be done, and count how many times that is as the number of dimes due. Once denominations of 10 can no longer be subtracted from the change due, the remaining value of the change due is pushed to step 5.
+        */
+    
+        /* Step 5. Subtract 5 from the remaining change due as many times as that can be done, and count how many times that is as the number of nickels due. Once denominations of 5 can no longer be subtracted from the change due, the remaining value of the change due is pushed to step 6.
+        */
+    
+        /* Step 6. Subtract 1 from the remaining change due as many times as that can be done, and count how many times that is as the number of pennies due. 1 can be subtracted from any remaining value of the change due until the value equals 0.
+        */
+    
+        /* Step 7. Print out, "Your change is: X dollar(s), X quarter(s), X dime(s), X nickel(s), and X cent(s)."
+        */
+    
+
+?>
+   
+   
+
+<!-- CHALLENGE 2 ----------------------------------------------------------------- -->
+
+
+
+<?php
+    /* Challenge 2a */
+    
+    
+    
+?>
+    
+    
+<?php
+    /* Challenge 2b */
+
+
+
+
+?>
+    
+    
+</body>
+</html>
