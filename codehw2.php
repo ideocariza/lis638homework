f64a0b3b6fc94bdff89bc29be58cbe41ffc46657
Izzy <ideocariza@gmail.com>
2018-02-18 19:14:44 -0500

    HW 2.0 - Challenge 1

---
+++ codehw2.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Code Homework 1</title>
+
+<?php
+    echo "<link rel='stylesheet' type='text/css' href='style-hw1.css' />";
+?>
+ 
+    
+</head>
+
+<body>
+
+<h1>Code Homework 2</h1>
+
+
+
+<!-- CHALLENGE 1 -------------------------------------------------- -->
+
+    <h3>Challenge 1</h3>
+    <h4>ISBN Validation</h4>
+
+<p>
+
+<?php
+    
+$isbn = "0747532699";
+
+$array = str_split($isbn);
+
+print_r($array);
+
+
+
+
+?>
+</p>
+
+
+<!-- CHALLENGE 2 --------------------------------------------------
+
+<h3>Challenge 2</h3>
+<h4>Coin Toss</i></h4>
+
+
+CHALLENGE 2A --------------------------------------------------
+
+<h4>A. 1, 3, 7, and 9 Flips.</i></h4>
+<p> 
+
+<?php
+        
+
+?>
+</p>
+
+
+CHALLENGE 2b ----------------------------------------------
+
+<h4>B. Toss Until Two Heads In a Row</i></h4>
+<p>
+
+-->
+
+<?php
+
+
+
+?>
+    
+</body>
+</html>
